7b6c727db73d109dacf3de8a53045e15b171397f
bys096 <82657858+bys096@users.noreply.github.com>
2023-06-03 10:51:06 +0900

    [refactor] line and format #2

--- frontend/bys_fenrir/src/components/SearchStroeVue.vue
+++ frontend/bys_fenrir/src/components/SearchStroeVue.vue
@@ -49,139 +49,137 @@
         </div>
       </div>
       <div class="stsch_navwrap">
-        <div class="stsch_nav_tlt">원하는 서비스를 선택하세요.</div>
-        <div class="stsch_nav">
-          <ul class="nav_top"></ul>
-          <ul></ul>
-        </div>
+        <div class="stsch_nav_tlt">必要なサービスを選択してください。</div>
+          <div class="stsch_nav">
+            <ul class="nav_top"></ul>
+            <ul></ul>
+          </div>
       </div>
-    
-      
     </div>
     
     
+
+    <div class="sch-result">
       <table class="table table-hover">
         <thead>
           <tr>
-            <td class="text-center">#</td>
+            <td class="text-center"></td>
             <td class="text-center">店舗名</td>
             <td class="text-center">住所/サービス</td>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(shop, index) in shopData" :key="index">
-            <td>
+            <td class="align-middle text-center">
               <div class="td-img-wrapper">
-                <img :src="shop.photo.pc.m" alt="">
+                <img :src="shop.photo.pc.m" alt="" class="img-thumbnail">
               </div>
             </td>
-            <td class="text-center">{{shop.name}}</td>
-            <td class="text-center">{{shop.address}}</td>
+            <td class="align-middle text-center">{{ shop.name }}</td>
+            <td class="align-middle text-center">{{ shop.address }}</td>
           </tr>
         </tbody>
       </table>
-      <br><br>
-      <div>
 
- <div class="text-center">
-    <!-- <v-pagination
-      v-model="page"
-      :length="pageCnt"
-      rounded="circle"
-    ></v-pagination> -->
+      <div class="map"></div>
+    </div>
 
-    <v-pagination
+    <div class="page-wrap">
+        <v-pagination
           v-model="page"
           :length="pageCnt"
           @input="pageLoad()"
-          total-visible="7"
-    class="my-pagination"
-      ></v-pagination>
-  </div>
-
-      </div>
+          total-visible="9"
+          class="my-pagination"
+        >
+        </v-pagination>
+    </div>
   </div>
 </template>
 
 <script>
+  import 'bootstrap'
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import axios from 'axios'
+  import { VPagination } from 'vuetify/lib';
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import axios from 'axios'
-import { VPagination } from 'vuetify/lib';
+  export default {
+    components: {
+      VPagination
+    },
 
+    data() {
+      return {
+        shopData: null,
+        keyword: null,
+        recordCnt: null,
+        pageCnt: null,
+        pageStart: 1,
+        pageSize: 5,
+        page: 1,
+        isActivePage: false
+      }
+    },
 
-export default {
-  components: {
-    VPagination
-  },
-  data() {
-    return {
-      shopData: null,
-      keyword: null,
-      recordCnt: null,
-      pageCnt: null,
-      pageStart: 1,
-      pageSize: 5,
-      page: 1,
-      isActivePage: false
-
-    }
-  },
-
-  methods: {
-    search() {
-      axios.get('/v1/?key=a6d3bb26218771ec&format=json&lat=35.7111&lng=139.7611&range=500',
-        {
+    methods: {
+      search() {
+        axios.get('/v1/?key=a6d3bb26218771ec&format=json&lat=35.7111&lng=139.7611&range=500', {
           params: {
             keyword: this.keyword,
             start: 0,
             count: this.pageSize
           }
-        }
-      )
-        .then((res) => {
-          console.log(res);
-          this.shopData = res.data.results.shop;
-          this.recordCnt = res.data.results.results_available;
-          this.pageCnt = this.recordCnt / 5;
-      });
-    },
-    pageLoad() {
-      // alert(this.page);
-      axios.get('/v1/?key=a6d3bb26218771ec&format=json&lat=35.7111&lng=139.7611&range=500',
-        {
+        })
+          .then((res) => {
+            console.log(res);
+            this.shopData = res.data.results.shop;
+            this.recordCnt = res.data.results.results_available;
+            this.pageCnt = this.recordCnt / 5;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+      pageLoad() {
+        axios.get('/v1/?key=a6d3bb26218771ec&format=json&lat=35.7111&lng=139.7611&range=500', {
           params: {
             keyword: this.keyword,
             start: this.pageSize * this.page + 1,
             count: this.pageSize
           }
-        }
-      )
-        .then((res) => {
-          console.log(res);
-          this.shopData = res.data.results.shop;
-      });
-      
+        })
+          .then((res) => {
+            console.log(res);
+            this.shopData = res.data.results.shop;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     }
   }
-}
 </script>
 
 <style scoped>
   @import url('https://netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome.css');
-  
+  @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
 
-    @import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
-
+  .stsch_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
   .stsch_box {
     border: 1px solid #cccccc;
     padding: 20px 57px 20px 60px;
   }
+  .page-wrap {
+    margin-top: 10vh;
+  }
   .stsch_form li {
     display: flex;
     flex-direction: row;
-    /* background-color: ; */
   }
   .stb_sel {
     display: flex;
@@ -189,7 +187,6 @@ export default {
   }
   .stb_sel select {
     margin-left: 1.3vw;
-    /* width: 100%; */
     width: 15vw;
     border: 1px solid #dddddd;
     color: #999999;
@@ -211,110 +208,74 @@ export default {
     margin-bottom: 2vh
   }
   .stsch_form .st_dot {
-      display: inline-block;
-      vertical-align: top;
-      width: 140px;
-      /* background: url(/_ui/desktop/common/images/gscvs/store/icon_store.png) no-repeat; */
-      color: #444;
-      font-size: 16px;
-      padding: 8px 0px 0px 12px;
+    display: inline-block;
+    vertical-align: top;
+    width: 140px;
+    color: #444;
+    font-size: 16px;
+    padding: 8px 0px 0px 12px;
   }
   .stb_btnwrap .sch {
-      padding: 12px 0;
-      width: 94px;
-      /* background: url(/_ui/desktop/common/images/gscvs/store/icon_store.png) no-repeat #397ca8; */
-      /* color: #fff; */
-      background-color: #ffdf65;
-      color: white;
-      width: 100px;
-      height: 78px;
-      text-align: center;
+    padding: 12px 0;
+    width: 94px;
+    background-color: #ffdf65;
+    color: white;
+    width: 100px;
+    height: 78px;
+    text-align: center;
   }
   .stb_btnwrap .sch {
-      padding: 12px 0;
-      width: 94px;
-      /* background: url(/_ui/desktop/common/images/gscvs/store/icon_store.png) no-repeat #397CB6; */
-      /* color: #fff; */
-      width: 100px;
-      height: 78px;
-      text-align: center;
+    padding: 12px 0;
+    width: 94px;
+    width: 100px;
+    height: 78px;
+    text-align: center;
   }
   .stsch_navwrap {
-      clear: both;
-      border-top: 1px dotted #cccccc;
+    clear: both;
+    border-top: 1px dotted #cccccc;
   }
   .stsch_navwrap .stsch_nav_tlt {
-      vertical-align: top;
-      /* background: url(/_ui/desktop/common/images/gscvs/store/icon_store.png) no-repeat; */
-      color: #444;
-      font-size: 16px;
-      padding: 8px 0px 0px 20px;
-      background-position: 10px 14px;
-      margin: 12px 0px 10px;
+    vertical-align: top;
+    color: #444;
+    font-size: 16px;
+    padding: 8px 0px 0px 20px;
+    background-position: 10px 14px;
+    margin: 12px 0px 10px;
   }
   .stsch_form .stsch_ipt {
-      width: 418px;
-      height: 34px;
-      border: 2px solid #666;
+    width: 418px;
+    height: 34px;
+    border: 2px solid #666;
   }
   input[type=text], input[type=password] {
-      text-indent: 5px;
-      border: 1px solid #d6d6d6;
-      height: 29px;
-      line-height: 31px;
-      color: #666;
-      font-size: 13px;
-  }
-
-  .stsch_container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    /* height: 100vh; */
+    text-indent: 5px;
+    border: 1px solid #d6d6d6;
+    height: 29px;
+    line-height: 31px;
+    color: #666;
+    font-size: 13px;
   }
   .fa-magnifying-glass {
     font-size: 5vmin;
   }
-
   table {
     width: 40vw;
     position: relative;
     right: 12vw;
     top: 5vh;
   }
-  /* td {
-    width: 20vw;
-    height: 20vh;
+  .td-img-wrapper {
+    width:20vw;
+    height:20vh;
   }
-  img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-
-      box-sizing: border-box;
-  } */
-  td {
-    width: 20vw;
-    height: 20vh;
-    overflow: hidden;  /* 셀의 크기를 넘어가는 내용을 숨김 */
-}
-
- .td-img-wrapper {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;  /* 셀의 크기를 넘어가는 이미지를 숨김 */
-}
-
-.td-img-wrapper img {
-    width: 100%;  /* 이미지를 셀의 너비에 맞게 조절 */
-    height: auto;  /* 이미지의 높이를 자동으로 조절하여 비율을 유지 */
-}
-
-
-.my-pagination >>> .v-pagination__item--active {
-  /* background-color: blue; */
-  color: black; /* 원하는 색상으로 변경하세요 */
-}
-
+  .td-img-wrapper img {
+      max-width:20vw;
+      max-height:20vh;
+      object-fit:cover;
+      cursor: pointer;
+  }
+  .my-pagination >>> .v-pagination__item--active {
+    color: black;
+  }
 </style>
