05f58419951e0758c5f1b8f7b9011160c2036d79
bys096 <82657858+bys096@users.noreply.github.com>
2023-06-03 12:37:14 +0900

    [add] map at search view #2
    - 開発したマップモジュールを検索画面に追加
    
    マップモジュールの機能：
    - 現在地取得
    - 現在地の位置情報をもとにマップを調整

--- frontend/bys_fenrir/src/main.js
+++ frontend/bys_fenrir/src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: {
+    // key: 'AIzaSyAil4xXCaukSc_hw2Bayr9DFrUf_T44N7A',
+    key: 'AIzaSyDNYcgyxV4LTXB5j2iwlq0YXv19a5dIOP8',
+    libraries: 'places', // 필요한 라이브러리 추가
+    language: 'ja'
+  },
+})
 
 new Vue({
   router,
