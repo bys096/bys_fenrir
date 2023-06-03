40a128b6b9d73b2e4ce44d7f55dea885789ab2c8
bys096 <82657858+bys096@users.noreply.github.com>
2023-06-03 17:39:11 +0900

    [add] spring project

---
+++ backend/guruguru_spring/src/main/java/com/fenrir/guruguru_spring/GuruguruSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fenrir.guruguru_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruguruSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruguruSpringApplication.class, args);
+    }
+
+}
