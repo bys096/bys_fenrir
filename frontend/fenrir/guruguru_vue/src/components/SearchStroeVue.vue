0dc0113b1ec5b00f6dba345010f1356fc61cdf4d
bys096 <82657858+bys096@users.noreply.github.com>
2023-06-10 23:19:00 +0900

    [add] search condition, [enhancement] search ui #29

--- frontend/fenrir/guruguru_vue/src/components/SearchStroeVue.vue
+++ frontend/fenrir/guruguru_vue/src/components/SearchStroeVue.vue
@@ -1,62 +1,89 @@
 <template>
   <div class="stsch_container">
-    <div class="stsch_box">
-      <div class="stsch">
-        <ul class="stsch_form">
-          <li>
-            <span class="st_dot">
-              <label for="stb1">地域区分</label>
-            </span>
-            <div class="stb_sel">
-              <div class="sel5_w">
-                <select name="" id="stb1" class="select1">
-                  <option value="" selected>地域選択</option>
-                </select>
+    <div class="brd">
+      <div class="border rounded-4 p-3 mb-2">
+        <div class="my-4">
+          <div class="row g-md-5">
+            <div class="col-lg-8 form-wrap">
+              <div class="position-relative my-4">
+                <div class="progress bg-warning" style="height: 3px">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                  </div>
+                </div>
+                <h2 class="
+                      position-absolute
+                      top-0
+                      start-50
+                      translate-middle
+                      bg-white
+                      text-center
+                      px-3
+                      fs-lg-4 fs-mobile-2
+                      fw-bold
+                    ">
+                  検索
+                </h2>
               </div>
-              <div class="sel5_w">
-                <select name="" id="stb1" class="select2">
-                  <option value="" selected>지역 선택</option>
-                </select>
-              </div>
-              <div class="sel5_w">
-                <select name="" id="stb1" class="select3">
-                  <option value="" selected>지역 선택</option>
-                </select>
-              </div>
+          
+              <form id="form" class="needs-validation" @submit.prevent="search">
+                <div class="mb-3">
+                  <label for="gps" class="form-label">現在地で検索</label>
+                  <v-checkbox label="現在地" id="gps" @click="findMyPlace" value=true></v-checkbox>
+                </div>
+
+                <div class="mb-3">
+                  <label for="email" class="form-label">店舗名</label>
+                  <input type="text" 
+                      class="form-control"
+                      id="email"
+                      name="userName" v-model="keyword"/>
+                </div>
+
+
+                <div class="mb-3">
+                  <label for="email" class="form-label">アドレス</label>
+                  <input type="text" 
+                      class="form-control"
+                      id="email"
+                      />
+                </div>
+
+                <div class="mb-3">
+                  <label for="email" class="form-label">サービス</label>
+                  <div class="service">
+                    <v-checkbox label="Wi-Fi" v-model="wifi" value="1"></v-checkbox>
+                    <v-checkbox label="食べ放題" v-model="free_food" value="1"></v-checkbox>
+                    <v-checkbox label="駐車場" v-model="parking" value="1"></v-checkbox>
+                    <v-checkbox label="ペット" v-model="pet" value="1"></v-checkbox>
+                    <v-checkbox label="ランチ" v-model="lunch" value="1"></v-checkbox>
+                    <v-checkbox label="ミッドナイト" v-model="midnight" value="1"></v-checkbox>
+                  </div>
+                </div>
+
+                <div class="d-none d-md-flex justify-content-center searchBtn">
+                  <span>
+                    
+                    <!-- 検索 -->
+                  </span>
+                  <button type="submit" class="
+                            btn btn-warning btn-lg btn-warning-hover
+                            px-5
+                            fw-bold
+                            rounded-pill
+                            mt-2
+                          "
+                  >
+                  <span class="search-icon"><svg-icon type="mdi" :path="path"></svg-icon></span>
+                    検索
+                  </button>
+                </div>
+              </form>
             </div>
-          </li>
-          <li>
-            <span class="st_dot">
-              <label for="stb1">地域区分</label>
-            </span>
-            <div class="stb_sel">
-              <div class="sel5_w">
-                <input type="text" class="stsch_ipt" v-model="keyword">
-              </div>  
-            </div>
-          </li>
-        </ul>
-        <div class="stb_btnwrap">
-          <div @click="search()" class="btn sch blue" id="searchButton">
-            <span>
-              <i class="fa-solid fa-magnifying-glass"></i>
-              <!-- 検索 -->
-            </span>
           </div>
-          <a href="" class="btn sch_all gray">
-            <span>詳細</span>
-          </a>
-        </div>
-      </div>
-      <div class="stsch_navwrap">
-        <div class="stsch_nav_tlt">必要なサービスを選択してください。</div>
-          <div class="stsch_nav">
-            <ul class="nav_top"></ul>
-            <ul></ul>
-          </div>
+        </div>    
       </div>
     </div>
-    
+
     <div class="sch-result">
       <table class="table table-hover store-list">
         <thead>
@@ -80,35 +107,28 @@
       </table>
 
       <div class="card">
-        
-        
-          <div class="map">
-        <!-- <div @click="test">btn</div> -->
-        <button @click="getCurrentPostion()">getCurrentPostion</button>
-        <p>{{textContent}}</p>
-        <gmap-map
-            :zoom="14"    
-            :center="center"
-            style="width:100%;  height: 600px;"
-        >
-        </gmap-map>
+        <div class="map">
+          <button @click="getCurrentPostion()">getCurrentPostion</button>
+          <p>{{textContent}}</p>
+          <gmap-map
+              :zoom="14"    
+              :center="center"
+              style="width:100%;  height: 600px;"
+          >
+          </gmap-map>
+        </div>
       </div>
-        
-      </div>
-
-      
     </div>
-
     <div class="page-wrap">
-        <v-pagination
-          v-model="page"
-          :length="pageCnt"
-          min="1"
-          @input="pageLoad()"
-          total-visible="9"
-          class="my-pagination"
-        >
-        </v-pagination>
+      <v-pagination
+        v-model="page"
+        :length="pageCnt"
+        min="1"
+        @input="pageLoad()"
+        total-visible="9"
+        class="my-pagination"
+      >
+      </v-pagination>
     </div>
   </div>
 </template>
@@ -118,10 +138,14 @@
   import 'bootstrap/dist/css/bootstrap.min.css'
   import axios from 'axios'
   import { VPagination } from 'vuetify/lib';
+  import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiMagnify } from '@mdi/js';
 
   export default {
     components: {
-      VPagination
+      VPagination,
+      SvgIcon
+
     },
     computed: {
       condition() {
@@ -130,6 +154,17 @@
           format: 'json',
           count: this.pageSize,
           start: this.pageSize * (this.page - 1) + 1,
+          address: '東京',
+          wifi: this.wifi,
+          free_food: this.free_food,
+          parking: this.parking,
+          pet: this.pet,
+          lunch: this.lunch,
+          midnight: this.midnight,
+          lat: this.center.lat,
+          lng: this.center.lng
+          // 35.652243448 lat
+          // 139.5444876358 lng
         }
       }
     },
@@ -137,7 +172,9 @@
     data() {
       return {
         shopData: null,
-        keyword: null,
+        path: mdiMagnify,
+
+        keyword: "",
         recordCnt: null,
         pageCnt: null,
         pageStart: 0,
@@ -148,13 +185,19 @@
         longitude: '',      // my longitude
         textContent: '',
         // center: { lat: 37.5642135, lng: 127.0016985 },
-        center: { lat: 35.7111, lng: 139.7611 },    // 
+        center: { lat: null, lng: null },    // 
         locationMarkers: [],
         locPlaces: [],
         existingPlace: null,
-        
-        
-        
+
+        wifi: null,
+        free_food: null,
+        parking: null,
+        pet: null,
+        lunch: null,
+        midnight: null,
+
+        isGps: false
       }
     },
 
@@ -164,7 +207,7 @@
         this.page = null;
         console.log('currentPage by Computed: ');
         console.log(this.condition);
-        axios.get('/v1/?key=a6d3bb26218771ec&lat=35.7111&lng=139.7611&range=500', {
+        axios.get('/v1/?key=a6d3bb26218771ec&range=500', {
           params: this.condition
         })
           .then((res) => {
@@ -173,6 +216,10 @@
             this.shopData = res.data.results.shop;
             this.recordCnt = res.data.results.results_available;
             this.pageCnt = Math.ceil(this.recordCnt / 4);
+
+            this.center.lat = this.shopData[0].lat;
+            this.center.lng = this.shopData[0].lng;
+
           })
           .catch((error) => {
             console.error(error);
@@ -180,7 +227,7 @@
       },
       pageLoad() {
         console.log('currnet page: ' + this.page);
-        axios.get('/v1/?key=a6d3bb26218771ec&lat=35.7111&lng=139.7611&range=500', {
+        axios.get('/v1/?key=a6d3bb26218771ec&range=500', {
           params: this.condition
         })
           .then((res) => {
@@ -205,6 +252,7 @@
           this.textContent = 'Your location data is ' + this.latitude + ', ' + this.longitude;
           this.center.lat = this.latitude;
           this.center.lng = this.longitude;
+          this.search();
           // : { lat: 35.7111, lng: 139.7611 }
         }, err => {
           this.textContent = err.message;
@@ -212,17 +260,20 @@
       },
 
       goToStoreDetail(shop) {
-        // $router.push('/store/detail');
         console.log('넘길 값');
         console.log(shop);
-        // this.$router.push({
-        //   path: `/store/detail`,
-        //   query: {
-        //     shop: shop
-        //   }
-        // });
         this.$store.commit('changeShop', shop);
         this.$router.push('/store/detail');
+      },
+      findMyPlace() {
+        this.isGps = !this.isGps;
+        if(this.isGps) {
+          this.getCurrentPostion();
+        } else {
+          this.center.lat = null;
+          this.center.lng = null;
+
+        }
       }
     }
   }
@@ -231,7 +282,11 @@
 <style scoped>
   @import url('https://netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome.css');
   
-
+.brd {
+  margin-top: 7vh;
+  width: 70vw;
+  min-width: 500px;
+}
   .stsch_container {
     display: flex;
     flex-direction: column;
@@ -239,8 +294,10 @@
     align-items: center;
   }
   .stsch_box {
+    /* margin-top: 10vh; */
     border: 1px solid #cccccc;
     padding: 20px 57px 20px 60px;
+    width: 60vw;
   }
   .sch-result {
     display: flex;
@@ -260,10 +317,11 @@
   .stb_sel {
     display: flex;
     flex-direction: row;
+    width: 30vw;
   }
   .stb_sel select {
     margin-left: 1.3vw;
-    width: 15vw;
+    width: 30vw;
     border: 1px solid #dddddd;
     color: #999999;
     font-size: 14px;
@@ -354,4 +412,24 @@
   .my-pagination >>> .v-pagination__item--active {
     color: black;
   }
+.address {
+  position: relative;
+  right: 20px;
+
+  /* color: red; */
+  
+}
+.service {
+  
+  display: flex;
+  gap: 2vw;
+  min-width: 450px;
+}
+.searchBtn button{
+  width: 70vw;
+}
+.search-icon {
+  position: relative;
+  bottom: 1px;
+}
 </style>
