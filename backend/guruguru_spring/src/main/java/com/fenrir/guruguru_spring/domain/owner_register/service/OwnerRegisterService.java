fab2cd2620821beb11ae770fb80041141bbf0586
bys096 <82657858+bys096@users.noreply.github.com>
2023-06-11 21:52:07 +0900

    [modify] upload file, register owner #32
    - オーナーが決定された店舗は登録できないように、例外処理を行いました。
    - ファイル選択時ではなく、店舗登録が送信されたタイミングでS3にアップロードされるように変更しました。

--- backend/guruguru_spring/src/main/java/com/fenrir/guruguru_spring/domain/owner_register/service/OwnerRegisterService.java
+++ backend/guruguru_spring/src/main/java/com/fenrir/guruguru_spring/domain/owner_register/service/OwnerRegisterService.java
@@ -2,6 +2,9 @@ package com.fenrir.guruguru_spring.domain.owner_register.service;
 
 import com.fenrir.guruguru_spring.domain.owner_register.Mapper.OwnerRegisterMapper;
 import com.fenrir.guruguru_spring.domain.owner_register.dto.OwnerRegisterRequestDto;
+import com.fenrir.guruguru_spring.domain.owner_register.entity.OwnerRegister;
+import com.fenrir.guruguru_spring.domain.owner_register.exception.OwnerDuplicateException;
+import com.fenrir.guruguru_spring.domain.owner_register.repository.OwnerRegisterFileRepository;
 import com.fenrir.guruguru_spring.domain.owner_register.repository.OwnerRegisterRepository;
 import com.fenrir.guruguru_spring.domain.store.entity.Store;
 import com.fenrir.guruguru_spring.domain.store.mapper.StoreMapper;
@@ -15,12 +18,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class OwnerRegisterService {
 
     private final OwnerRegisterRepository ownerRegisterRepository;
+    private final OwnerRegisterFileRepository ownerRegisterFileRepository;
     private final UserRepository userRepository;
     private final OwnerRegisterMapper ownerRegisterMapper;
     private final StoreMapper storeMapper;
@@ -28,25 +34,35 @@ public class OwnerRegisterService {
 
     public void registerStore(OwnerRegisterRequestDto dto) throws BusinessException {
 
+        log.info("register store service 진입");
+        log.info(dto.toString());
+
         User user = userRepository.findById(SecurityUtil.getCurrentMemberId())
                         .orElseThrow(() -> {
                             throw new UserNotFoundException();
                         });
+        Optional<OwnerRegister> ownerRegister = ownerRegisterRepository.findByStore_StoreCode(dto.getStoreCode());
+        ownerRegister.ifPresent(o -> {
+            throw new OwnerDuplicateException();
+        });
 
-        // 가게가 이미 같은 가게를 가지고 있을 경우의 예외처리 필요
 
-
+        log.info("가게 중복검사 완료");
         storeRepository.findByStoreCode(dto.getStoreCode())
                         .ifPresentOrElse(
                                 store -> {
-                                    ownerRegisterRepository.save(ownerRegisterMapper.toEntity(user, store));
+                                    log.info("owner 등록 시작");
+                                    OwnerRegister owner = ownerRegisterRepository.save(ownerRegisterMapper.toEntity(user, store));
+                                    log.info("owner 파일등록 시작");
+                                    ownerRegisterFileRepository.save(ownerRegisterMapper.toEntity(dto.getFileName(), owner));
                                 },
                                 () -> {
                                     Store store = storeRepository.save(storeMapper.toEntity(dto.getStoreCode(), dto.getStoreName()));
                                     log.info("실행됨?");
                                     log.info(store.getStoreName());
 
-                                    ownerRegisterRepository.save(ownerRegisterMapper.toEntity(user, store));
+                                    OwnerRegister owner =  ownerRegisterRepository.save(ownerRegisterMapper.toEntity(user, store));
+                                    ownerRegisterFileRepository.save(ownerRegisterMapper.toEntity(dto.getFileName(), owner));
                                 }
                         );
     }
